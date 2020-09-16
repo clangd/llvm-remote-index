43a250b26b130b5dbc2683863af0dcda4a29fdcd
Kadir Cetinkaya <kadircet@google.com>
2020-09-16 16:10:10 +0200

    Introduce container creation
    Downloads latest index-server release and bundles it with artifact
    downloader script to periodically(every 6 hours) download llvm index.
    
    Built docker image, sets up a cron job for downloading the index and
    then runs the clangd-index-server on default port.
    
    Hopefully all of this infra can be used for any other project, only by
    setting repo to fetch the index and asset name.

---
+++ deployment/entry_point.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INDEX_FILE="/index.idx"
+INDEX_FETCHER_CMD="/index_fetcher.sh $REPOSITORY $INDEX_ASSET_PREFIX $INDEX_FILE"
+
+# Abort script on failure.
+set -e
+# Print commands as we execute them.
+set -x
+
+# Run index fetcher once every 6 hours.
+echo "0 */6 * * * $INDEX_FETCHER_CMD" > crontab_schedule.txt
+crontab crontab_schedule.txt
+cron
+
+# Fetch index once.
+$INDEX_FETCHER_CMD
+
+# Start the server.
+/clangd-index-server $INDEX_FILE $INDEXER_PROJECT_ROOT
