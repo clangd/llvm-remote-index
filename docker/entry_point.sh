837e2cf72db85b9ea7d42934c7bab8ffc6bc91c9
Kadir Cetinkaya <kadircet@google.com>
2021-05-04 10:26:39 +0200

    Introduce status page generation
    This patch registers status_updater.sh to cron to run every minute.
    
    `status_updater.sh` queries index-server instances running on current
    machine at mentioned ports. It uses `clangd-index-server-monitor` for
    querying instances and uses `j2cli` to fillout templates under
    `status_templates/` using the response from instances and environment
    variables.
    
    In the end this produces an html file that displays the VM instance name
    with port next to information about the index its serving (currently
    only its age) and instance's uptime.

--- docker/entry_point.sh
+++ docker/entry_point.sh
@@ -2,21 +2,27 @@
 
 INDEX_FILE="/index.idx"
 INDEX_FETCHER_CMD="/index_fetcher.sh $REPOSITORY $INDEX_ASSET_PREFIX $INDEX_FILE"
+STATUS_UPDATER_CMD="/status_updater.sh $PROJECT_NAME"
+INSTANCE_PORTS="50051"
+STATUS_UPDATER_CMD="${STATUS_UPDATER_CMD} ${INSTANCE_PORTS}"
 
 # Abort script on failure.
 set -e
 # Print commands as we execute them.
 set -x
 
+# Set the path so that cron can find j2.
+echo "PATH=$PATH" > crontab_schedule.txt
 # Run index fetcher once every 6 hours.
-echo "0 */6 * * * $INDEX_FETCHER_CMD" > crontab_schedule.txt
+echo "0 */6 * * * $INDEX_FETCHER_CMD" >> crontab_schedule.txt
+# Update status every minute.
+echo "* * * * * $STATUS_UPDATER_CMD" >> crontab_schedule.txt
 crontab crontab_schedule.txt
 cron
 
-# Start the nginx server. Contents in /var/www/html are served at *:80.
-service nginx start
-
-# Fetch index once.
+# Run status updater at startup to generate error file.
+$STATUS_UPDATER_CMD
+# Fetch index once to start serving immediately.
 $INDEX_FETCHER_CMD
 
 # Start the nginx server. Contents in /var/www/html are served at *:80.
---
+++ docker/status_updater.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Issues monitoring requests on all ports of the localhost passed in as
+# positional args and merges those into one final html.
+
+TMPL_DIR="/status_templates"
+HEADER_TMPL="$TMPL_DIR/header"
+SUCCESS_TMPL="$TMPL_DIR/success"
+FAILURE_TMPL="$TMPL_DIR/failure"
+FOOTER_TMPL="$TMPL_DIR/footer"
+OUT_FILE="/var/www/html/index.html"
+
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 PROJECT_NAME PORTS..."
+  echo "  PROJECT_NAME - e.g. llvm-remote-index"
+  echo "  PORTS... - One or more ports on the localhost serving an index-server"
+  exit
+fi
+
+set -e -x
+
+# Stores monitoring info from grpc servers.
+TEMP_DATA_FILE=$(mktemp)
+
+# Current output we are building, swapped with $OUT_FILE once it is complete.
+TEMP_OUT_FILE=$(mktemp)
+# Make sure it will be readable once moved to final target.
+chmod a+r $TEMP_OUT_FILE
+
+# Delete tmp files on exit.
+trap "rm -f $TEMP_DATA_FILE" EXIT
+trap "rm -f $TEMP_OUT_FILE" EXIT
+
+# Env variables used by templates.
+export HOST_NAME="$(hostname -s)"
+export PROJECT_NAME="$1"
+shift
+
+j2 $HEADER_TMPL >> $TEMP_OUT_FILE
+
+# All the remaining args are ports on the local machine to connect.
+while [[ $# -gt 0 ]];
+do
+  PORT="$1"
+  shift
+  export INSTANCE_NAME="${HOST_NAME}:${PORT}"
+
+  if /clangd-index-server-monitor "localhost:${PORT}" > $TEMP_DATA_FILE; then
+    TMPL_FILE=$SUCCESS_TMPL
+  else
+    TMPL_FILE=$FAILURE_TMPL
+    # j2 expects a valid json file, so in case of failure to communicate with
+    # server, just provide an empty json.
+    echo '{}' > $TEMP_DATA_FILE
+  fi
+
+    j2 --format=json -e '' "$TMPL_FILE" "$TEMP_DATA_FILE" >> $TEMP_OUT_FILE
+done
+
+j2 $FOOTER_TMPL >> $TEMP_OUT_FILE
+
+mv $TEMP_OUT_FILE $OUT_FILE
